1caef2bd4c1e8cda45901f90e5a7cee19909b6a7
bkcatton <ben.k.catton@gmail.com>
2022-04-28 15:55:03 -0700

    feat - add tuples

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,9 @@ let isPublished: boolean = true;
 let x: any = 'Hello'
 let age: number;
 
+let ids: number[] = [1,2,3,4,5] // array of numbers
+let arr: any[] = [1, true, 'hi'] // array of any types
+
+
+//tuples
+let person: [number, string, boolean] = [1, 'Ben', true];
